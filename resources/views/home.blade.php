f23fc39bebd82675a2e05e2aeb45ea09b6fb554c
SantiVigna <ynotvigna@gmail.com>
2024-11-25 14:47:37 +0100

    build: initial view structure

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,24 @@
-@extends('')
+@extends('layouts.app')
+
+@section('content')
+    <div class="journals">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Date</th>
+                    <th scope="col">Entry</th>
+                    <th scope="col">Emotion</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($employments as $employment)
+                    <tr>
+                        <td>{{$employment->created_at}}</td>
+                        <td>{{$employment->entry}}</td>
+                        <td><img src="{{asset($employment->emotion)}}" class="emotionImg" alt="emotion"></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
