b92117d7d045d91a5d3969a12c0605265e438c71
SantiVigna <ynotvigna@gmail.com>
2024-11-28 11:21:44 +0100

    build: first footer structure

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,22 @@
 <footer>
-    <p>Inside Out App © 2024 Equipo 2. All Rights Reserved. </p>
+    <section>
+        <table>
+            <tr>
+                <td>
+                    <a href="https://github.com/SantiVigna">
+                        <img src="{{ asset('img/githubLogo.png') }}" alt="">
+                        SantiVigna_
+                    </a>
+                </td>
+                <td>
+                    <a href="https://www.instagram.com/santivigna_/">
+                        <img src="{{ asset('img/instagramLogo.png') }}" alt="">
+                        SantiVigna_
+
+                    </a>
+                </td>
+            </tr>
+        </table>
+    <p>© 2024, Santino Vigna. All rights reserved.</p>
+</section>
 </footer>
